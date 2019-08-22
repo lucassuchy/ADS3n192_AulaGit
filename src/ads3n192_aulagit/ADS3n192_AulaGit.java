1d6ff1a47e6d5efa0c355a266833c78177fd24a2
FELIPE FRIEDRICH SANTOS <631910142 @fspoa.br>
2019-08-21 21:46:55 -0300

    B alterou x

--- src/ads3n192_aulagit/ADS3n192_AulaGit.java
+++ src/ads3n192_aulagit/ADS3n192_AulaGit.java
@@ -16,7 +16,7 @@ public class ADS3n192_AulaGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String X = "2";
+        String X = "25";
     }
     
 }
